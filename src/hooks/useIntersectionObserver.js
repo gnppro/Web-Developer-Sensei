58f337e9133689c12708a30e4e0d006e6f2d509f
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-02-05 19:15:57 -0600

    ✨ [New] useIntersectionObserver Hook

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export default function useIntersectionObserver(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const observer = new IntersectionObserver(
+    ([entry]) => setIntersecting(entry.isIntersecting),
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return isIntersecting;
+}
