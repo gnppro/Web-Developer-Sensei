aba834b59aa922d8e73038f4b6e347fbf08800f1
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-02-05 19:17:03 -0600

    ✨ [Introduce] App API fetch

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
+import Items from "../containers/Items";
+import Loader from "../components/Loader/Loader";
+import status from "../constants/status";
+import "./App.css";
+
+const { IDLE, SUCCESS, FAILURE, LOADING } = status;
+
+const API_URL = "https://api.fbi.gov/wanted/v1/list";
 
 function App() {
+  const ref = useRef();
+  const onScreen = useIntersectionObserver(ref);
+  const [data, setData] = useState({});
+  const [allItems, setAllItems] = useState([]);
+  const [status, setStatus] = useState(IDLE);
+  const { total, page } = data || {};
+  const hasMore = page < total;
+
+  const cleanup = () => {};
+
+  useEffect(() => {
+    getData(page + 1);
+    return cleanup();
+  }, []);
+
+  useEffect(() => {
+    if (hasMore && onScreen) getData(page + 1);
+  }, [onScreen]);
+
+  const getData = (page) => {
+    setStatus(LOADING);
+    fetch(`${API_URL}?page=${page}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setAllItems((prevItems) => [...prevItems, ...data.items]);
+        setStatus(SUCCESS);
+      })
+      .catch((error) => {
+        setStatus(FAILURE);
+      });
+  };
+
+  const renderData = () => {
+    switch (status) {
+      case IDLE:
+        return;
+      case LOADING:
+        return (
+          <div className="Loader-Container">
+            <Loader />
+          </div>
+        );
+      case SUCCESS:
+        return <Items items={allItems} />;
+      case FAILURE:
+        return <p>Error</p>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: "center" }}>FBI - MOST WANTED</h1>
+      {renderData()}
+      <div ref={ref} style={{ display: "flex", justifyContent: "center" }}>
+        {/* {status === LOADING && allItems.length > 0 && <Loader />} */}
+      </div>
     </div>
   );
 }
