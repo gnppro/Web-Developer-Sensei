cee8d53ccea22356bc3cbbb1cee36e923c10f346
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-02-05 19:13:52 -0600

    ✨ [New] Button Component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,29 @@
+import "./Button.css";
+
+const Button = ({ children, onClick }) => {
+  function createRipple(event) {
+    const button = event.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+    onClick();
+  }
+
+  return <button onClick={createRipple}>{children}</button>;
+};
+
+export default Button;
